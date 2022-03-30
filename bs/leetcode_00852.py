afd3f75d0716d2fcefbad79c00955b6523aa4d87
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-03-31 00:51:20 +0900

    leetcode 852

---
+++ bs/leetcode_00852.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left = 1
+        right = len(arr) - 2
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid - 1] < arr[mid] and arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            elif arr[mid - 1] > arr[mid] and arr[mid] > arr[mid + 1]:
+                right = mid - 1
+            else:
+                return mid
