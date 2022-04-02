9f0ea9a134f492fd0092b36a9004465b79258f1b
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-04-02 19:03:23 +0900

    dress in vest

--- two_pointers/dress_in_vest.py
+++ two_pointers/dress_in_vest.py
@@ -1,25 +1,24 @@
-num_soliders, num_vests, lower, upper = list(map(int, input().split()))
-soliders = list(map(int, input().split()))
-vests = list(map(int, input().split()))
-
+num_soliders, num_vests,\
+                lower_bound, upper_bound = list(map(int, input().split()))
+solider_sizes = list(map(int, input().split()))
+vest_sizes = list(map(int, input().split()))
+ 
 vest_pointer = 0
 solider_pointer = 0
-
 results = []
-
 while (vest_pointer < num_vests) and (solider_pointer < num_soliders):
-    smallest = soliders[solider_pointer] - lower
-    biggest = soliders[solider_pointer + 1] + upper
-    if vests[vest_pointer] >= smallest and vests[vest_pointer] <= biggest:
+    smallest_size = solider_sizes[solider_pointer] - lower_bound
+    biggest_size = solider_sizes[solider_pointer] + upper_bound
+    if vest_sizes[vest_pointer] >= smallest_size\
+        and vest_sizes[vest_pointer] <= biggest_size:
         results.append((solider_pointer + 1, vest_pointer + 1))
         vest_pointer += 1
         solider_pointer += 1
-    elif vests[vest_pointer] < smallest:
-        vest_pointer += 1
-    elif vests[vest_pointer] > biggest:
+    elif biggest_size < vest_sizes[vest_pointer]:
         solider_pointer += 1
-        
+    elif smallest_size > vest_sizes[vest_pointer]:
+        vest_pointer += 1
+ 
 print(len(results))
 for result in results:
     print(result[0], result[1])
-
