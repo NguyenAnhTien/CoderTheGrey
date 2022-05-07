f048a64d6d9e5e5640c388f4485f722448cb17db
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-05-07 16:04:02 +0900

    leetcode 2008

--- bs/leetcode_02008.py
+++ bs/leetcode_02008.py
@@ -1,28 +1,15 @@
-from bisect import bisect_left
-from functools import lru_cache
-
-def maxTaxiEarnings(n, rides):
-    # @lru_cache(None)
-    def dp(index, ans):
-        if index == n:
-            return 0
-        start, end, tip = rides[index]
-        # ans = dp(index + 1)
-        j = bisect_left(rides, [end, 0, 0])
-        # print("before: ", f"index : {index}", end, start, tip, j, ans)
-        # if index == 3:
-        #     import pdb; pdb.set_trace()
-        value = dp(j, ans)
-        print(f"index: {index} j: {j} dp(j): {value}")
-        ans = max(ans, end - start + tip + value)
-        # print("after: ", f"index : {index}", end, start, tip, j, ans)
-        return ans
-    
-    n = len(rides)
-    ans = 0
-    rides.sort()
-    return dp(0, ans)
-
-n = 20
-rides = [[1,6,1],[3,10,2],[10,12,3],[11,12,2],[12,15,2],[13,18,1]]
-maxTaxiEarnings(n, rides)
+class Solution:
+    def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
+        @lru_cache(None)
+        def dp(index):
+            if index == n:
+                return 0
+            start, end, tip = rides[index]
+            ans = dp(index + 1)
+            j = bisect_left(rides, [end, 0, 0])
+            ans = max(ans, end - start + tip + dp(j))
+            return ans
+        
+        n = len(rides)
+        rides.sort()
+        return dp(0)
