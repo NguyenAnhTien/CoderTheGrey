a1a9269d49791676b29a20ea601afebdabb7536b
Tien Nguyen Anh <tien.nguyenanh94@gmail.com>
2022-05-22 16:16:37 +0900

    leetcode 125

--- palindrome/leetcode_00125.py
+++ palindrome/leetcode_00125.py
@@ -5,17 +5,24 @@ def isPalindrome(s):
         if char.isalpha() or char.isnumeric():
             clean_str += char.lower()
 
-    center = (0 + len(clean_str)) // 2
+    center = (0 + len(clean_str) - 1) // 2
     left = center
     right = center
-    while left <= 0 and right < len(clean_str) and clean_str[left] == clean_str[right]:
+    if len(clean_str) % 2 == 0:
+        right = center + 1
+    while left >= 0 and right < len(clean_str) and clean_str[left] == clean_str[right]:
         left -= 1
         right += 1
 
-    if (right - left -1) != len(clean_str):
+    if (right - left -1) == len(clean_str):
         return True
     return False
 
 if __name__ == '__main__':
-    s = "race a car"
-    print(isPalindrome(s))
+    inputs = ["A man, a plan, a canal: Panama",
+            "aa",
+            "cabbac",
+            "race a car",
+            " "]
+    for s in inputs:
+        print(isPalindrome(s))
